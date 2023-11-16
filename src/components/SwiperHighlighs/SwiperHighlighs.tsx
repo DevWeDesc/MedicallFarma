c2eb8a4c1590cfdddacf50ff4418c3f3f4d8c339
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 09:53:06 -0300

    feat: Consumindo dados mockados dos produtos

--- src/components/SwiperHighlighs/SwiperHighlighs.tsx
+++ src/components/SwiperHighlighs/SwiperHighlighs.tsx
@@ -5,13 +5,30 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import "./styles.css";
+import { HighlightsProductsData } from "../../../data/data";
+import Image from "next/image";
 
 export default function SwiperHighlighs() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 1</SwiperSlide>
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        slidesPerView={2.1}
+        className="mySwiper"
+      >
+        {HighlightsProductsData.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="flex flex-col gap-2">
+              <Image alt="" src={item.image} />
+              <div>
+                <p className="text-xs font-medium min-h-[32px]">{item.name}</p>
+                <p className="text-xs font-light pr-3">{item.content}</p>
+                <p className="text-xs font-medium">{item.supplier}</p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
