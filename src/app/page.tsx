ed56289e7d6ae61b3a327fd05489169bda30e34a
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 09:25:15 -0300

    feat: Adicionando Componentes no Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Aboult } from "@/components/Aboult";
+import { Banner } from "@/components/Banner";
 import { Header } from "@/components/Header";
+import { HighlightsProducts } from "@/components/HighlightsProducts";
+
 export default function Home() {
   return (
     <div>
       <Header />
-      <main className=""></main>
+      <main className="">
+        <Banner />
+        <Aboult />
+        <HighlightsProducts />
+      </main>
     </div>
   );
 }
