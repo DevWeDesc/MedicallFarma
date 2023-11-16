09ee8a974aeed413ebb27f2c63529996776c4e7f
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 09:52:42 -0300

    feat: Criando Data e Types dos produtos

---
+++ data/data.ts
@@ -0,0 +1,42 @@
+import { IProductsData } from "../types/types";
+import Product1 from "../public/ImageProduct1.png";
+import Product2 from "../public/ImageProduct2.png";
+import Product3 from "../public/ImageProduct3.png";
+
+export const HighlightsProductsData: IProductsData[] = [
+  {
+    id: 1,
+    image: Product1,
+    name: "Amoxicilina",
+    content:
+      "Amoxicilina é um antibiótico amplamente utilizado para tratar infecções bacterianas.",
+    supplier: "Fornecido por Nativita",
+  },
+  {
+    id: 2,
+
+    image: Product2,
+    name: "Estetoscópio Simples Adulto Premium",
+    content:
+      "Amoxicilina é um antibiótico amplamente utilizado para tratar infecções bacterianas.",
+    supplier: "Fornecido por Nativita",
+  },
+  {
+    id: 3,
+
+    image: Product3,
+    name: "Trophic Basic Enteral",
+    content:
+      "Amoxicilina é um antibiótico amplamente utilizado para tratar infecções bacterianas.",
+    supplier: "Fornecido por Nativita",
+  },
+  {
+    id: 4,
+
+    image: Product1,
+    name: "Amoxicilina",
+    content:
+      "Amoxicilina é um antibiótico amplamente utilizado para tratar infecções bacterianas.",
+    supplier: "Fornecido por Nativita",
+  },
+];
---
+++ types/types.ts
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image";
+
+export type IProductsData = {
+  id: number;
+  image: StaticImageData | string;
+  name: string;
+  content: string;
+  contentCompleted?: string;
+  description?: string;
+  categories?: ICategoriesProducts[];
+  supplier: string;
+};
+
+type ICategoriesProducts = {
+  category: string;
+};
