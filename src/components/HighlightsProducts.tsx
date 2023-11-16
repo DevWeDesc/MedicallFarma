cecd01c49046894680f2db5bfca555ec190fefa8
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 13:41:36 -0300

    style: refatorando estilos

--- src/components/HighlightsProducts.tsx
+++ src/components/HighlightsProducts.tsx
@@ -3,7 +3,7 @@ import SwiperHighlighs from "./SwiperHighlighs/SwiperHighlighs";
 
 export const HighlightsProducts = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 pb-14">
       <h2 className="font-bold text-2xl">Produtos em Destaque</h2>
       <div className="h-2 w-16 bg-dark rounded-full" />
       <SwiperHighlighs />
--- src/components/SwiperHighlighs/SwiperHighlighs.tsx
+++ src/components/SwiperHighlighs/SwiperHighlighs.tsx
@@ -7,6 +7,7 @@ import { Navigation } from "swiper/modules";
 import "./styles.css";
 import { HighlightsProductsData } from "../../../data/data";
 import Image from "next/image";
+import { CardProduct } from "../CardProduct";
 
 export default function SwiperHighlighs() {
   return (
@@ -19,14 +20,13 @@ export default function SwiperHighlighs() {
       >
         {HighlightsProductsData.map((item) => (
           <SwiperSlide key={item.id}>
-            <div className="flex flex-col gap-2">
-              <Image alt="" src={item.image} />
-              <div>
-                <p className="text-xs font-medium min-h-[32px]">{item.name}</p>
-                <p className="text-xs font-light pr-3">{item.content}</p>
-                <p className="text-xs font-medium">{item.supplier}</p>
-              </div>
-            </div>
+            <CardProduct
+              id={item.id}
+              content={item.content}
+              image={item.image}
+              name={item.name}
+              supplier={item.supplier}
+            />
           </SwiperSlide>
         ))}
       </Swiper>
