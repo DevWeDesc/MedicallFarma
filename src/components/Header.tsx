63fa298ad2de527a4cba0024f01b6b3a7546b7b5
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 09:24:20 -0300

    feat: Criando componente de Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,11 @@ export const Header = () => {
   return (
     <div className="flex items-center justify-between bg-grayDark px-2 py-5">
       <Image alt="" src={Logo} height={28} />
+      <div className="w-6 flex flex-col gap-[6px]">
+        <div className="h-[1.5px] bg-white" />
+        <div className="h-[1.5px] bg-white" />
+        <div className="h-[1.5px] bg-white" />
+      </div>
       <Nav />
     </div>
   );
