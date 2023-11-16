ed20511844a3a1606830c4b90ac74ad03a3ba720
ViniHGV <ixxvinicius@gmail.com>
2023-11-16 09:25:32 -0300

    feat: criando SwiperHighlightsProducts

---
+++ src/components/SwiperHighlighs/SwiperHighlighs.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import "./styles.css";
+
+export default function SwiperHighlighs() {
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
