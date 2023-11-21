f899ca82b65db1467dde4c1d2cdd3dd76fe604b7
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 11:29:35 -0300

    Editando typagem da description,  IProductsData

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,4 @@
+import { Url } from "next/dist/shared/lib/router/router";
 import { StaticImageData } from "next/image";
 
 export type IProductsData = {
@@ -6,9 +7,10 @@ export type IProductsData = {
   name: string;
   content: string;
   contentCompleted?: string;
-  description?: string;
+  description?: { title: string; content: string }[];
   categories?: string[];
   supplier: string;
+  size?: string;
 };
 
 // type ICategoriesProducts = {
@@ -18,6 +20,7 @@ export type IProductsData = {
 export type ICardSoluctions = {
   title: string;
   image: StaticImageData | string;
+  href: string;
 };
 
 export type ICategories = {
