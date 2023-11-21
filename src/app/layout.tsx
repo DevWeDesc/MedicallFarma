5733aa22f3cf9950ffd104b78f4ee87d9c94c9cc
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 11:25:14 -0300

    Adicionando Footer ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const robotoCondesed = Roboto_Condensed({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={robotoCondesed.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
