5f1631598c6da67f3daa716cf15ba55b008a9d60
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 08:12:46 -0300

    Adicionando imagens de fornecedores default

--- src/components/Suppliers.tsx
+++ src/components/Suppliers.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import React from "react";
+import ImageSuplliers from "../../public/ImageSupplier.png";
 
 export const Suppliers = () => {
   return (
-    <div className="py-12">
+    <div className="py-12 flex flex-col gap-2">
       {" "}
       <h2 className="text-2xl font-bold text-center">Nossos Fornecedores</h2>
+      <Image alt="" src={ImageSuplliers}></Image>
     </div>
   );
 };
