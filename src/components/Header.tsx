6f725eb15c4531e9153955c31fdfbe59c45f0302
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 11:27:55 -0300

    Adicionando Linkagem a homePage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import React from "react";
 import Logo from "../../public/Logo.png";
 import { Nav } from "./Nav";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <div className="flex items-center justify-between bg-grayDark px-2 py-5">
-      <Image alt="" src={Logo} height={28} />
+      <Link href="/">
+        <Image alt="" src={Logo} height={28} />
+      </Link>
       <div className="w-6 flex flex-col gap-[6px]">
         <div className="h-[1.5px] bg-white" />
         <div className="h-[1.5px] bg-white" />
