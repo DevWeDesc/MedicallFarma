603d87ed96b40f4bb9976f23e323838508371970
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 08:09:15 -0300

    Adicionando header ao layout para exibição default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
 
 const robotoCondesed = Roboto_Condensed({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={robotoCondesed.className}>{children}</body>
+      <body className={robotoCondesed.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
