1fd340ebd7e79a114e5b59deecebfd8bf8d9e0c5
ViniHGV <ixxvinicius@gmail.com>
2023-11-21 11:28:33 -0300

    Adicionando Linkagem e editando estilização

--- src/components/ProductsRecommended.tsx
+++ src/components/ProductsRecommended.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { ProductsRecomendedData } from "../../data/data";
 import { CardProduct } from "./CardProduct";
+import Link from "next/link";
 
 export const ProductsRecommended = () => {
   return (
     <div className="py-12 px-5 flex flex-col gap-5">
       <h2 className="text-xl font-bold">Produtos Recomendados para Você!</h2>
-      <div className="grid grid-cols-3 gap-y-6">
+      <div className="grid grid-cols-3 gap-3">
         {ProductsRecomendedData.map((item) => (
-          <CardProduct
-            key={item.id}
-            id={item.id}
-            content={item.content}
-            image={item.image}
-            name={item.name}
-            supplier={item.supplier}
-          />
+          <Link href={`/detailsProduct/${item.id}`} key={item.id}>
+            <CardProduct
+              key={item.id}
+              id={item.id}
+              content={item.content}
+              image={item.image}
+              name={item.name}
+              supplier={item.supplier}
+            />
+          </Link>
         ))}
       </div>
     </div>
