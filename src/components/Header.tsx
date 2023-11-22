3295fe4c155af8c4f83abb088c91db0d49a9a69b
ViniHGV <ixxvinicius@gmail.com>
2023-11-22 09:26:40 -0300

    Adicionando SearchBar ao Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,45 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../public/Logo.png";
 import { Nav } from "./Nav";
 import Link from "next/link";
+import { IoIosSearch } from "react-icons/io";
+import { SearchBar } from "./SearchBar";
 
 export const Header = () => {
+  const [searchOpen, setSearchOpen] = useState(false);
+
+  const handleSearchOpen = () => {
+    setSearchOpen(false);
+  };
+
   return (
-    <div className="flex items-center justify-between bg-grayDark px-2 py-5">
+    <div
+      className={`flex items-center justify-between gap-4 bg-grayDark px-2  ${
+        searchOpen ? "py-3" : "py-4"
+      }`}
+    >
       <Link href="/">
-        <Image alt="" src={Logo} height={28} />
+        <Image alt="" src={Logo} height={21} />
       </Link>
-      <div className="w-6 flex flex-col gap-[6px]">
-        <div className="h-[1.5px] bg-white" />
-        <div className="h-[1.5px] bg-white" />
-        <div className="h-[1.5px] bg-white" />
+      {searchOpen && (
+        <div className={`w-[60%] ${searchOpen ? "col-span-3" : ""}`}>
+          <SearchBar handleSearchOpen={handleSearchOpen} />
+        </div>
+      )}
+      <div className="flex items-center gap-5">
+        {!searchOpen && (
+          <IoIosSearch
+            onClick={() => setSearchOpen(true)}
+            className="text-2xl text-white"
+          />
+        )}
+        <div className="w-6 flex flex-col gap-[6px]">
+          <div className="h-[1.5px] bg-white" />
+          <div className="h-[1.5px] bg-white" />
+          <div className="h-[1.5px] bg-white" />
+        </div>
       </div>
       <Nav />
     </div>
