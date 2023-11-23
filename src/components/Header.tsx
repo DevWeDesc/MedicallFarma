bbd0b59fcfb89da8ed59c5e021ae54039b8b6feb
ViniHGV <ixxvinicius@gmail.com>
2023-11-23 08:11:11 -0300

    Adicionando Funcionalidade de menu hamburguer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,39 +9,54 @@ import { SearchBar } from "./SearchBar";
 
 export const Header = () => {
   const [searchOpen, setSearchOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleSearchOpen = () => {
     setSearchOpen(false);
   };
 
+  const handleMenuOpen = () => {
+    menuOpen ? setMenuOpen(false) : setMenuOpen(true);
+  };
+
   return (
     <div
-      className={`flex items-center justify-between gap-4 bg-grayDark px-2  ${
+      className={`xl:px-28 fixed z-30 w-full flex items-center justify-between gap-4 bg-grayDark px-2  ${
         searchOpen ? "py-3" : "py-4"
       }`}
     >
       <Link href="/">
-        <Image alt="" src={Logo} height={21} />
+        <Image alt="" src={Logo} className="h-5 w-auto xl:h-14" />
       </Link>
-      {searchOpen && (
-        <div className={`w-[60%] ${searchOpen ? "col-span-3" : ""}`}>
-          <SearchBar handleSearchOpen={handleSearchOpen} />
-        </div>
-      )}
+
+      <div
+        className={`w-[60%] ${searchOpen ? "col-span-3" : "hidden xl:block"}`}
+      >
+        <SearchBar handleSearchOpen={handleSearchOpen} />
+      </div>
+
       <div className="flex items-center gap-5">
         {!searchOpen && (
           <IoIosSearch
             onClick={() => setSearchOpen(true)}
-            className="text-2xl text-white"
+            className="text-2xl text-white xl:hidden"
           />
         )}
-        <div className="w-6 flex flex-col gap-[6px]">
-          <div className="h-[1.5px] bg-white" />
-          <div className="h-[1.5px] bg-white" />
-          <div className="h-[1.5px] bg-white" />
+        <div onClick={handleMenuOpen} className="w-6 flex flex-col gap-[6px] ">
+          <div
+            className={`h-[1.5px] bg-white transition-all ${
+              menuOpen ? "rotate-45 top-1 relative" : ""
+            }`}
+          />
+          <div className={`h-[1.5px] bg-white ${menuOpen ? "hidden" : ""}`} />
+          <div
+            className={`h-[1.5px] bg-white transition-all ${
+              menuOpen ? "-rotate-45 relative bottom-1" : ""
+            }`}
+          />
         </div>
       </div>
-      <Nav />
+      <Nav menuOpen={menuOpen} handleMenuOpen={() => setMenuOpen(false)} />
     </div>
   );
 };
