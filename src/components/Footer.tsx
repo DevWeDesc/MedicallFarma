37f3bfbbeb82453894f6c754e22d1cacfb053613
ViniHGV <ixxvinicius@gmail.com>
2023-11-23 08:13:27 -0300

    Refatorando Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,33 @@ import Image from "next/image";
 import React from "react";
 import Logo from "../../public/Logo.png";
 import { NavData } from "../../data/data";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="bg-grayDark py-16 flex flex-col items-center gap-16 text-white">
+    <div
+      id="Contact"
+      className="bg-grayDark py-16 flex flex-col items-center gap-16 text-white"
+    >
       <Image alt="" src={Logo} />
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold">Mapa do Site</h3>
         <div className="flex flex-col gap-2">
           {NavData.map((item, index) => (
-            <p key={index} className="text-sm font-light">
+            <Link href={item.href} key={index} className="text-sm font-light">
               {item.name}
-            </p>
+            </Link>
           ))}
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-xl font-bold text-center">Nossas Redes Sociais</h3>
         <div className="flex gap-2">
-          {NavData.map((item, index) => (
+          {/* {NavData.map((item, index) => (
             <p key={index} className="text-sm font-light">
               {item.name}
             </p>
-          ))}
+          ))} */}
         </div>
       </div>
       <p className="text-center text-sm">
