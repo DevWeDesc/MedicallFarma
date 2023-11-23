f26f1695ce4667652c775843c709e13758e0c303
ViniHGV <ixxvinicius@gmail.com>
2023-11-23 11:51:10 -0300

    Arrumando tela de destaques do produto

--- src/app/detailsProduct/[id]/page.tsx
+++ src/app/detailsProduct/[id]/page.tsx
@@ -18,25 +18,29 @@ export default function Page({ params }: { params: { id: number } }) {
   console.log(productSelected);
 
   return (
-    <div className="pt-20 px-5 flex flex-col gap-10 mb-14">
+    <div className="pt-20 px-5 flex flex-col gap-10 mb-14 lg:px-28">
       <div>
         <div className="my-6 text-sm">
-          <Link href="/">Voltar para Home</Link>
+          <Link href="/" className="lg:text-base">
+            Voltar para Home
+          </Link>
         </div>
-        <CardProduct
-          size="large"
-          content={productSelected.content}
-          id={productSelected.id}
-          image={productSelected.image}
-          name={productSelected.name}
-          supplier={productSelected.supplier}
-          categories={productSelected.categories}
-        />
+        <div className="lg:pr-28">
+          <CardProduct
+            size="large"
+            content={productSelected.content}
+            id={productSelected.id}
+            image={productSelected.image}
+            name={productSelected.name}
+            supplier={productSelected.supplier}
+            categories={productSelected.categories}
+          />
+        </div>
       </div>
       {productSelected.description && (
         <div>
-          <h3 className="text-xl font-medium mb-2">Descrição</h3>
-          <div className="flex flex-col gap-3 text-sm">
+          <h3 className="text-xl font-medium mb-2 lg:text-4xl">Descrição</h3>
+          <div className="flex flex-col gap-3 text-sm lg:text-xl">
             {productSelected?.description?.map((item, index) => (
               <div key={index}>
                 <h4 className="font-medium">{item.title}</h4>
--- src/components/Cards/CardProduct.tsx
+++ src/components/Cards/CardProduct.tsx
@@ -23,38 +23,46 @@ export const CardProduct = ({
   return (
     <div
       className={`flex flex-col text-left ${
-        size === "large" ? "gap-4 items-center" : "gap-2"
+        size === "large"
+          ? "gap-4 items-center lg:grid lg:grid-cols-2 lg:justify-items-center"
+          : "gap-2"
       } gap-2`}
     >
-      <Image alt="" src={image} />
+      <Image
+        alt=""
+        className={`${size === "large" ? "lg:w-4/5" : ""}`}
+        src={image}
+      />
       <div className={`${size === "large" && "flex flex-col gap-2"}`}>
         <p
           className={`font-medium min-h-[32px] lg:min-h-[48px] ${
-            size === "large" ? "text-2xl" : "text-xs lg:text-base"
+            size === "large" ? "text-2xl lg:text-5xl" : "text-xs lg:text-base"
           }`}
         >
           {name}
         </p>
         <p
           className={`font-light pr-3 ${
-            size === "large" ? "text-sm opacity-70" : "text-xs lg:text-sm"
+            size === "large"
+              ? "text-sm opacity-70 lg:text-xl"
+              : "text-xs lg:text-sm"
           }`}
         >
           {contentProduct}
         </p>
         <p
           className={`font-medium  ${
-            size === "large" ? "text-base" : "text-xs lg:text-base"
+            size === "large" ? "text-base lg:text-xl" : "text-xs lg:text-base"
           }`}
         >
           {supplier}
         </p>
         {size === "large" && (
-          <div className="mt-4 flex flex-col gap-2">
-            <button className="w-full rounded-full py-2 bg-grayLight hover:bg-grayDefault transition-all text-white ">
+          <div className="mt-4 lg:mt-10 flex flex-col gap-2">
+            <button className="w-full rounded-full py-2 lg:text-xl bg-grayLight hover:bg-grayDefault transition-all lg:max-w-[315px] text-white ">
               Solicitar um Orçamento
             </button>
-            <p className="font-medium">
+            <p className="font-medium lg:text-xl">
               Categorias:{" "}
               {categories?.map((item, index) => {
                 let TextCategories =
