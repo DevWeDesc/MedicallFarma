eb736d197e768afa03ef8b359b647cd930ee5a8f
ViniHGV <ixxvinicius@gmail.com>
2023-11-30 12:46:40 -0300

    refactor: refatorando Supplits

--- src/components/Suppliers.tsx
+++ src/components/Suppliers.tsx
@@ -1,23 +1,20 @@
-import Image from "next/image";
 import React from "react";
-import ImageSuplliers from "../../public/ImageSupplier.png";
-import ImageSuplliersDesktop from "../../public/imageSupplierDesktop.png";
 import SwiperSuppliers from "./SwiperSuppliers/SwiperSuppliers";
 
-export const Suppliers = () => {
+type ISuppliers = {
+  title: string;
+  content?: string;
+};
+
+export const Suppliers = ({ title, content }: ISuppliers) => {
   return (
-    <div className="py-12 flex flex-col gap-2 lg:gap-6 lg:py-28">
+    <div className="px-5 py-12 flex flex-col gap-2 lg:gap-6 lg:py-28">
       {" "}
-      <h2 className="text-2xl font-bold text-center lg:text-5xl">
-        Nossos Fornecedores
-      </h2>
+      <h2 className="text-3xl font-bold text-center lg:text-5xl">{title}</h2>
+      <p className="font-light opacity-75 text-sm lg:text-base text-center max-w-3xl m-auto">
+        {content}
+      </p>
       <SwiperSuppliers />
-      {/* <Image
-        alt=""
-        className="hidden lg:block w-full"
-        src={ImageSuplliersDesktop}
-      ></Image>
-      <Image alt="" className="lg:hidden" src={ImageSuplliers}></Image> */}
     </div>
   );
 };
