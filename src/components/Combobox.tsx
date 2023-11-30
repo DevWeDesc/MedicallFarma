02dfe74ef0bfa23169e64f46ee873e65831180e9
ViniHGV <ixxvinicius@gmail.com>
2023-11-30 12:45:28 -0300

    refactor: refatorando Combobox

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -1,47 +1,73 @@
 "use client";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
+import { CategoriesData } from "../../data/data";
+import { ICategories } from "../../types/types";
+import { CiPlay1 } from "react-icons/ci";
 
-type ICombobox = {
-  Category: string;
-  SubCategories: string[];
-  ImageProp: StaticImageData | string;
-};
-
-export const Combobox = ({ Category, SubCategories, ImageProp }: ICombobox) => {
+export const Combobox = () => {
   const [comboOpen, setComboOpen] = useState(false);
+  const [categorySelected, setCategorySelected] = useState<ICategories>();
 
-  const handleComboOpen = () => {
-    comboOpen ? setComboOpen(false) : setComboOpen(true);
+  const handleComboOpen = (item: ICategories) => {
+    if (comboOpen) {
+      setComboOpen(false);
+      setCategorySelected(item);
+    } else {
+      setCategorySelected(item);
+      setComboOpen(true);
+    }
   };
 
   return (
-    <div className="bg-grayDark text-white">
-      <button
-        onMouseLeave={handleComboOpen}
-        onMouseOver={handleComboOpen}
-        className="p-4 bg-grayDark"
-      >
-        {Category}
-      </button>
+    <div className="hidden xl:flex flex-col pt-20 ">
+      <div className="bg-grayDark text-white flex gap-10 justify-center">
+        {CategoriesData.map((item, index) => (
+          <button
+            key={index}
+            onMouseLeave={() => handleComboOpen(item)}
+            onMouseOver={() => handleComboOpen(item)}
+            className={`p-8 bg-grayDark hover:font-bold transition-all ${
+              categorySelected?.name === item.name && "font-bold"
+            }`}
+          >
+            {item.name}
+          </button>
+        ))}
+      </div>
       <div
-        onMouseLeave={handleComboOpen}
-        className={`absolute w-full max-w-[860px] m-auto left-1/4 ${
-          SubCategories.length > 12
-            ? "grid grid-cols-3 justify-items-center gap-y-5 gap-x-10"
-            : SubCategories.length > 6
-            ? "grid grid-cols-2 gap-y-5 gap-x-10"
-            : "grid grid-cols-2 gap-y-5 gap-x-10"
-        } ${comboOpen ? " bg-grayLight transition-all" : "-z-10 hidden"}`}
+        className="relative flex w-full justify-center"
+        onMouseLeave={() => {
+          setComboOpen(false);
+          setCategorySelected(categorySelected);
+        }}
+        onMouseOver={() => {
+          setComboOpen(true);
+          setCategorySelected(categorySelected);
+        }}
       >
-        <div className="p-20">
-          {SubCategories.map((item, index) => (
-            <p key={index} className="cursor-pointer">
-              {item}
-            </p>
-          ))}
-        </div>
-        <Image alt="" className="w-full h-full" src={ImageProp} />
+        {comboOpen && categorySelected && (
+          <div className="absolute bg-grayLight py-10 px-6 rounded-md grid grid-cols-2 text-white">
+            <div className="flex flex-col gap-6 ">
+              {categorySelected?.subCategories.map((item, index) => (
+                <Link
+                  href="/Medicamentos"
+                  key={index}
+                  className="flex items-center gap-3 opacity-90 hover:opacity-100 hover:font-semibold transition-all"
+                >
+                  <CiPlay1 />
+                  <p>{item}</p>
+                </Link>
+              ))}
+            </div>
+            <Image
+              alt=""
+              className="max-w-sm"
+              src={categorySelected?.image}
+            ></Image>
+          </div>
+        )}
       </div>
     </div>
   );
