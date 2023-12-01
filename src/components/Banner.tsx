d956e8b6fcbb2a6a3af554e12f84c1f34b003c3b
ViniHGV <ixxvinicius@gmail.com>
2023-12-01 11:15:22 -0300

    Style: editando estilização

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,23 +1,29 @@
 import React from "react";
-import ImageBanner from "../../public/ImageBanner.png";
+// import ImageBanner from "../../public/ImageBanner.png";
+import ImageBanner from "../../public/imageBannerProducts.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Banner = () => {
   return (
-    <div className="bg-grayDark px-5 flex flex-col-reverse gap-8 py-16 xl:grid xl:grid-cols-2 xl:items-center xl:gap-28 xl:pl-28 xl:pt-24 2xl:pl-80">
+    <div
+      // className="bg-grayDark px-5 flex flex-col-reverse gap-8 py-16 xl:grid xl:grid-cols-2 xl:items-center xl:gap-28 xl:pl-28 xl:pt-24 2xl:pl-80"
+      className="bg-grayDark px-5 flex flex-col gap-8 py-16 xl:items-center xl:gap-28 xl:pt-24"
+    >
       <div className="text-white flex flex-col gap-8">
-        <h2 className="max-w-[387px] text-3xl font-bold xl:max-w-md xl:text-5xl">
-          Encontre os melhores Medicamentos com os Melhores Preços!
+        <h2 className="max-w-[387px] text-2xl xl:max-w-4xl font-light text-center xl:text-5xl">
+          Encontre os melhores Produtos Hospitalares com os Melhores Preços aqui
+          na <span className="font-bold">Medicall Farma!</span>
         </h2>
-        <Link
+        {/* <Link
           href="#productRecomended"
           className="xl:max-w-md text-center rounded-full py-3 px-10 font-bold text-xl hover:bg-greenDark transition-all bg-greenDefault"
         >
           Procurar Medicamentos
-        </Link>
+        </Link> */}
       </div>
       <Image alt="" src={ImageBanner} />
+      {/* <Image alt="" src={ImageBanner} /> */}
     </div>
   );
 };
