7f371d3082debba21eb8c7e30c87a875e0da65f9
ViniHGV <ixxvinicius@gmail.com>
2023-12-12 13:48:08 -0300

    fix: editando informações do card, tipagem e forma de pegar os produtos em destaque

--- src/components/SwiperHighlighs/SwiperHighlighs.tsx
+++ src/components/SwiperHighlighs/SwiperHighlighs.tsx
@@ -4,12 +4,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Navigation } from "swiper/modules";
 import "./styles.css";
-import { HighlightsProductsData } from "../../../data/data";
+import { HighlightsProductsData, ListProductsData } from "../../../data/data";
 import { CardProduct } from "../Cards/CardProduct";
 import Link from "next/link";
+import { IListProductsData } from "../../../types/types";
 
 export default function SwiperHighlighs() {
   const [slidesPerView, setSlidesPerView] = useState(2.1);
+  const highlightsProducts: IListProductsData[] = ListProductsData.slice(8, 16);
 
   useEffect(() => {
     if (window) {
@@ -26,7 +28,7 @@ export default function SwiperHighlighs() {
         style={{ padding: "10px" }}
         className="mySwiper"
       >
-        {HighlightsProductsData.map((item) => (
+        {highlightsProducts.map((item) => (
           <SwiperSlide key={item.id}>
             <Link
               className="hover:scale-105 transition-all"
@@ -34,9 +36,11 @@ export default function SwiperHighlighs() {
             >
               <CardProduct
                 id={item.id}
-                content={item.content}
+                product={item.product}
                 image={item.image}
-                name={item.name}
+                category={item.category}
+                href={item.href}
+                subCatgegory={item.subCatgegory}
                 supplier={item.supplier}
               />
             </Link>
