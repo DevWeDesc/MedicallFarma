ec709c8aed9c2548edbbe17db1623de37a57f712
ViniHGV <ixxvinicius@gmail.com>
2023-12-12 13:44:36 -0300

    refactor: refatorando página de filtro por subcategorias

--- src/app/[category]/[subcategory]/page.tsx
+++ src/app/[category]/[subcategory]/page.tsx
@@ -5,28 +5,31 @@ import { CardProduct } from "@/components/Cards/CardProduct";
 import Link from "next/link";
 import {
   CardSoluctionData,
+  ListProductsData,
   ProductsRecomendedData,
 } from "../../../../data/data";
-import { IProductsData } from "../../../../types/types";
-import { StaticImageData } from "next/image";
+import { IListProductsData, IProductsData } from "../../../../types/types";
+import Image, { StaticImageData } from "next/image";
 import ImageDietaEnteral from "../../../../public/ImageDietasEnterais.png";
 import ImageAcessories from "../../../../public/ImageAcessoriesMedic.png";
 import ImageMedicaments from "../../../../public/ImageMedicaments.png";
 
 export default function Page() {
   const { category, subcategory } = useParams();
-  let categoryString = subcategory.toString().includes("%20")
-    ? subcategory.toString().replace("%20", " ")
-    : subcategory.toString();
-  const [ProductsCategory, setProductsCategory] = useState<IProductsData[]>([]);
+  const categoryString = subcategory.toString();
+  let titlePage = categoryString.replace(/-/g, " ");
+  titlePage = titlePage
+    .substring(0, 1)
+    .toUpperCase()
+    .concat(titlePage.substring(1));
+  const ProductsCategory: IListProductsData[] = ListProductsData.filter(
+    (item) => item.href.includes(categoryString)
+  );
   const [ImagePage, setImagePage] = useState<StaticImageData | string>("");
 
   useEffect(() => {
-    let productsFilteredCategory: IProductsData[] =
-      ProductsRecomendedData.filter((item) =>
-        item.categories?.includes(categoryString)
-      );
-    setProductsCategory(productsFilteredCategory);
+    // let productsFilteredCategory: IListProductsData[] =
+    // setProductsCategory(productsFilteredCategory);
 
     // if (filterSubCategorie.length > 0) {
     //   productsFilteredCategory = productsFilteredCategory.filter((item) => {
@@ -45,21 +48,20 @@ export default function Page() {
       : setImagePage(ImageAcessories);
   }, [category, ImagePage]);
 
-  console.log("categ", category);
-  console.log(subcategory);
+  console.log(ProductsCategory);
   return (
     <div>
       <div className="pt-14 lg:pt-20 relative bg-grayDark text-white">
-        {/* <Image alt="" className="-z-10 w-full" src={ImagePage} /> */}
+        <Image alt="" className="-z-10 w-full" src={ImagePage} />
         <Link
           href="/"
           className="absolute top-[20%] px-4 text-xs -translate-y-3/4 lg:px-28 lg:text-sm 2xl:px-80"
         >
           Voltar para Home
         </Link>
-        <div className="absolute top-2/4 -translate-y-2/4 px-5 grid gap-4 grid-cols-3 lg:px-28 lg:gap-20 2xl:px-80">
+        <div className="absolute top-1/2 transform -translate-y-1/2 px-5 grid gap-4 grid-cols-3 lg:px-28 lg:gap-20 2xl:px-80">
           <div className="h-full flex flex-col justify-center font-medium">
-            <h2 className="text-lg lg:text-5xl">{category}</h2>
+            <h2 className="text-lg lg:text-5xl">{titlePage}</h2>
             <p className="text-xs lg:text-3xl">
               Avanços e Desafios na Indústria
             </p>
@@ -92,14 +94,16 @@ export default function Page() {
           </>
         )}
         <div className="2xl:max-w-7xl 2xl:m-auto flex flex-col gap-10">
-          <h2 className="font-bold text-2xl lg:text-5xl">{subcategory}</h2>
+          <h2 className="font-bold text-2xl lg:text-5xl">{titlePage}</h2>
           <div className="grid grid-cols-3 gap-4 lg:grid-cols-5 lg:gap-8  ">
             {ProductsCategory.map((item) => (
               <Link href={`/detailsProduct/${item.id}`} key={item.id}>
                 <CardProduct
-                  content={item.content}
+                  category={item.category}
                   image={item.image}
-                  name={item.name}
+                  product={item.product}
+                  subCatgegory={item.subCatgegory}
+                  href={item.href}
                   id={item.id}
                   supplier={item.supplier}
                 />
