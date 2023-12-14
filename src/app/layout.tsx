0fff79b1e300525f7d274892fd5a1520c286f430
ViniHGV <ixxvinicius@gmail.com>
2023-12-14 08:30:38 -0300

    feat: adicionando Icone de whatsapp com redirecionamento para api do whatsapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { BsWhatsapp } from "react-icons/bs";
+
+import Link from "next/link";
 
 const robotoCondesed = Roboto_Condensed({ subsets: ["latin"] });
 
@@ -23,6 +26,14 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Link
+          target="_blank"
+          className="fixed z-30"
+          href="https://api.whatsapp.com/send?phone=5511997271350&text=Olá!%20estou%20interessado%20em%20pedir%20um%20orçamento!"
+          title="Link para Entrar em contato com o WhatsApp de Orçamentos"
+        >
+          <BsWhatsapp className="fixed bottom-10 right-10 text-4xl  cursor-pointer text-green-500 hover:text-green-600 transition-all" />
+        </Link>
       </body>
     </html>
   );
