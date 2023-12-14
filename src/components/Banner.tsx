12cd11e27a88fee0dfcd8769e8a4e6e99dead946
ViniHGV <ixxvinicius@gmail.com>
2023-12-14 08:32:47 -0300

    fix: editando imagem do banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import ImageBanner from "../../public/ImageBanner.png";
-import ImageBanner from "../../public/imageBannerProducts.png";
+import ImageBanner from "../../public/GroupProductsBanner.svg";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,10 +8,10 @@ export const Banner = () => {
   return (
     <div
       // className="bg-grayDark px-5 flex flex-col-reverse gap-8 py-16 xl:grid xl:grid-cols-2 xl:items-center xl:gap-28 xl:pl-28 xl:pt-24 2xl:pl-80"
-      className="bg-grayDark px-5 flex flex-col gap-8 py-16 xl:items-center xl:gap-28 xl:pt-24"
+      className="bg-grayDark px-5 flex flex-col gap-8 py-16 sm:pt-24 xl:items-center xl:gap-12 xl:pt-24"
     >
       <div className="text-white flex flex-col gap-8">
-        <h2 className="max-w-[387px] text-2xl xl:max-w-4xl font-light text-center xl:text-5xl">
+        <h2 className="max-w-[387px] m-auto text-2xl sm:text-3xl xl:max-w-4xl font-light text-center xl:text-5xl">
           Encontre os melhores Produtos Hospitalares com os Melhores Preços aqui
           na <span className="font-bold">Medicall Farma!</span>
         </h2>
@@ -22,7 +22,7 @@ export const Banner = () => {
           Procurar Medicamentos
         </Link> */}
       </div>
-      <Image alt="" src={ImageBanner} />
+      <Image alt="" className="m-auto" src={ImageBanner} />
       {/* <Image alt="" src={ImageBanner} /> */}
     </div>
   );
