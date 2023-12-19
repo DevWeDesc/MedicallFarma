c95d2066a2134ea221487a45d4c60f20ce08cc84
ViniHGV <ixxvinicius@gmail.com>
2023-12-19 13:40:23 -0300

    feat: criando teste unitario do componente SoluctionsMoment

---
+++ src/tests/SoluctionsMoment.spec.tsx
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { SolutionsMoment } from "@/components/SolutionsMoment";
+import "intersection-observer";
+
+describe("SolutionsMoment render", () => {
+  it("renders SolutionsMoment unchanged", () => {
+    const { container } = render(<SolutionsMoment />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders SolutionsMoment link unchanged", () => {
+    const { container } = render(<SolutionsMoment />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Solutions Moment Text contain", () => {
+    const { getByText } = render(<SolutionsMoment />);
+
+    expect(getByText("Soluções do Momento!"));
+    expect(getByText("Saneantes")).toBeInTheDocument();
+    expect(getByText("Tratamentos de ferida")).toBeInTheDocument();
+    expect(getByText("Material")).toBeInTheDocument();
+  });
+});
