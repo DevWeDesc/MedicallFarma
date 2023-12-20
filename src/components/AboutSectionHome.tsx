29d8e88af1e916497e6c30a3ac91ce00c434811a
ViniHGV <ixxvinicius@gmail.com>
2023-12-20 11:29:32 -0300

    feat: criando componente ancorado a página de aboult

---
+++ src/components/AboutSectionHome.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import imageMedicall from "../../public/imagemedicall.jpeg";
+import Image from "next/image";
+import Link from "next/link";
+import { AnimationScroll } from "@/animations/AnimationScroll";
+
+export const AboutSectionHome = () => {
+  return (
+    <AnimationScroll>
+      <div className="flex flex-col justify-center relative items-center bg-black text-white">
+        <Image
+          alt=""
+          className="w-full object-cover h-80 opacity-60"
+          src={imageMedicall}
+        />
+        <div className="absolute h-full w-full flex flex-col justify-center gap-8 items-center">
+          <p className="text-2xl font-bold">
+            Conheça mais sobre a MEDICALL FARMA
+          </p>
+          <Link
+            className="text-center border w-auto px-8 py-2 rounded-lg hover:font-semibold hover:border-[2px] transition-all"
+            href="about"
+          >
+            Saiba Mais!
+          </Link>
+        </div>
+      </div>
+    </AnimationScroll>
+  );
+};
